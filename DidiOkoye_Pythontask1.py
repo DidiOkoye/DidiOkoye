73ba56dcc27a7c0e2a401d3a42168b09e51a9f21
DidiOkoye <61988963+DidiOkoye@users.noreply.github.com>
2020-03-26 12:25:58 +0100

    My python task

---
+++ DidiOkoye_Pythontask1.py
@@ -0,0 +1,8 @@
+pi = 3.14
+print('Hello There!')
+print('Input value of Radius below to generate Area of Circle')
+radius = input('radius: ')
+area_circle = pi * int(radius) ** 2
+print('Area of Circle =')
+print(area_circle)
+print('There you go!')
